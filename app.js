0db5d1b717d7b7451f44ef2e63181f69b90455ce
Charlie Heiner <heinercj@gmail.com>
2017-11-13 13:47:24 -0800

    started constructor function. changed links to nav bars

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+'use strict';
+const answersArray = ['yes', 'no', 'absolutely', 'possibly', 'highly unlikly', 'who knows'];
 
+function fortuneTell (name){
+    this.name = name;
+    this.answers = answersArray;
+    this.userAnswers = [];
+}
+
+function randomAnswer(){
+    if(userAnswers.length > 0){
+        const userAns = userAnswers[Math.floor(Math.random() * userAnswers.length)];
+        return userAns;
+
+    }else{
+        const ans = answersArray[Math.floor(Math.random() * answersArray.length)];
+        return ans;
+    }
+}
