487244a082e6f159219c2284b4c261f91b3fdd20
missalissa <alissa.beddow@gmail.com>
2017-11-14 16:20:23 -0800

    experimentation with animation

--- animation.js
+++ animation.js
@@ -1,43 +1,279 @@
-//https://dribbble.com/shots/2706997-The-Future
+// //https://dribbble.com/shots/2706997-The-Future
 
-$(document).ready(function(){
-    var theLetters = "abcdefghijklmnopqrstuvwxyz&^$!"; //You can customize what letters it will cycle through
-    var ctnt = "Arturo Cabrera"; // Your text 
-    var speed = 20; // ms per frame
-    var increment = 10; // frames per step. Must be >2
+// $(document).ready(function(){
+//     var theLetters = "abcdefghijklmnopqrstuvwxyz&^$!"; //You can customize what letters it will cycle through
+//     var ctnt = "Yes"; // Your text 
+//     var speed = 20; // ms per frame
+//     var increment = 10; // frames per step. Must be >2
     
         
-    var clen = ctnt.length;       
-    var si = 0;
-    var stri = 0;
-    var block = "";
-    var fixed = "";
-    //Call self x times, whole function wrapped in setTimeout
-    (function rustle (i) {          
-    setTimeout(function () {
-      if (--i){rustle(i);}
-      nextFrame(i);
-      si = si + 1;        
-    }, speed);
-    })(clen*increment+1); 
-    function nextFrame(pos){
-      for (var i=0; i<clen-stri; i++) {
-        //Random number
-        var num = Math.floor(theLetters.length * Math.random());
-        //Get random letter
-        var letter = theLetters.charAt(num);
-        block = block + letter;
-      }
-      if (si == (increment-1)){
-        stri++;
-      }
-      if (si == increment){
-      // Add a letter; 
-      // every speed*10 ms
-      fixed = fixed +  ctnt.charAt(stri - 1);
-      si = 0;
-      }
-      $(".name").html(fixed + block);
-      block = "";
+//     var clen = ctnt.length;       
+//     var si = 0;
+//     var stri = 0;
+//     var block = "";
+//     var fixed = "";
+//     //Call self x times, whole function wrapped in setTimeout
+//     (function rustle (i) {          
+//         setTimeout(function () {
+//             if (--i){rustle(i);}
+//             nextFrame(i);
+//             si = si + 1;        
+//         }, speed);
+//     })(clen*increment+1); 
+//     function nextFrame(pos){
+//         for (var i=0; i<clen-stri; i++) {
+//         //Random number
+//             var num = Math.floor(theLetters.length * Math.random());
+//             //Get random letter
+//             var letter = theLetters.charAt(num);
+//             block = block + letter;
+//         }
+//         if (si == (increment - 1)){
+//             stri++;
+//         }
+//         if (si == increment){
+//             // Add a letter; 
+//             // every speed*10 ms
+//             fixed = fixed +  ctnt.charAt(stri - 1);
+//             si = 0;
+//         }
+//         $(".name").html(fixed + block);
+//         block = "";
+//     }
+// });
+function WordShuffler(holder,opt){
+    var that = this;
+    var time = 0;
+    this.now;
+    this.then = Date.now();
+    
+    this.delta;
+    this.currentTimeOffset = 0;
+    
+    this.word = null;
+    this.currentWord = null;
+    this.currentCharacter = 0;
+    this.currentWordLength = 0;
+  
+  
+    var options = {
+      fps : 20,
+      timeOffset : 5,
+      textColor : '#000',
+      fontSize : "50px",
+      useCanvas : false,
+      mixCapital : false,
+      mixSpecialCharacters : false,
+      needUpdate : true,
+      colors : [
+        '#f44336','#e91e63','#9c27b0',
+        '#673ab7','#3f51b5','#2196f3',
+        '#03a9f4','#00bcd4','#009688',
+        '#4caf50','#8bc34a','#cddc39',
+        '#ffeb3b','#ffc107','#ff9800',
+        '#ff5722','#795548','#9e9e9e',
+        '#607d8b'
+      ]
     }
+  
+    if(typeof opt != "undefined"){
+      for(key in opt){
+        options[key] = opt[key];
+      }
+    }
+  
+  
+    
+    this.needUpdate = true;
+    this.fps = options.fps;
+    this.interval = 1000/this.fps;
+    this.timeOffset = options.timeOffset;
+    this.textColor = options.textColor;
+    this.fontSize = options.fontSize;
+    this.mixCapital = options.mixCapital;
+    this.mixSpecialCharacters = options.mixSpecialCharacters;
+    this.colors = options.colors;
+  
+     this.useCanvas = options.useCanvas;
+    
+    this.chars = [
+      'A','B','C','D',
+      'E','F','G','H',
+      'I','J','K','L',
+      'M','N','O','P',
+      'Q','R','S','T',
+      'U','V','W','X',
+      'Y','Z'
+    ];
+    this.specialCharacters = [
+      '!','§','$','%',
+      '&','/','(',')',
+      '=','?','_','<',
+      '>','^','°','*',
+      '#','-',':',';','~'
+    ]
+  
+    if(this.mixSpecialCharacters){
+      this.chars = this.chars.concat(this.specialCharacters);
+    }
+  
+    this.getRandomColor = function () {
+      var randNum = Math.floor( Math.random() * this.colors.length );
+      return this.colors[randNum];
+    }
+  
+    //if Canvas
+   
+    this.position = {
+      x : 0,
+      y : 50
+    }
+  
+    //if DOM
+    if(typeof holder != "undefined"){
+      this.holder = holder;
+    }
+  
+    if(!this.useCanvas && typeof this.holder == "undefined"){
+      console.warn('Holder must be defined in DOM Mode. Use Canvas or define Holder');
+    }
+  
+  
+    this.getRandCharacter = function(characterToReplace){    
+      if(characterToReplace == " "){
+        return ' ';
+      }
+      var randNum = Math.floor(Math.random() * this.chars.length);
+      var lowChoice =  -.5 + Math.random();
+      var picketCharacter = this.chars[randNum];
+      var choosen = picketCharacter.toLowerCase();
+      if(this.mixCapital){
+        choosen = lowChoice < 0 ? picketCharacter.toLowerCase() : picketCharacter;
+      }
+      return choosen;
+      
+    }
+  
+    this.writeWord = function(word){
+      this.word = word;
+      this.currentWord = word.split('');
+      this.currentWordLength = this.currentWord.length;
+  
+    }
+  
+    this.generateSingleCharacter = function (color,character) {
+      var span = document.createElement('span');
+      span.style.color = color;
+      span.innerHTML = character;
+      return span;
+    }
+  
+    this.updateCharacter = function (time) {
+      
+        this.now = Date.now();
+        this.delta = this.now - this.then;
+  
+         
+  
+        if (this.delta > this.interval) {
+          this.currentTimeOffset++;
+        
+          var word = [];
+  
+          if(this.currentTimeOffset === this.timeOffset && this.currentCharacter !== this.currentWordLength){
+            this.currentCharacter++;
+            this.currentTimeOffset = 0;
+          }
+          for(var k=0;k<this.currentCharacter;k++){
+            word.push(this.currentWord[k]);
+          }
+  
+          for(var i=0;i<this.currentWordLength - this.currentCharacter;i++){
+            word.push(this.getRandCharacter(this.currentWord[this.currentCharacter+i]));
+          }
+  
+  
+          if(that.useCanvas){
+            c.clearRect(0,0,stage.x * stage.dpr , stage.y * stage.dpr);
+            c.font = that.fontSize + " sans-serif";
+            var spacing = 0;
+            word.forEach(function (w,index) {
+              if(index > that.currentCharacter){
+                c.fillStyle = that.getRandomColor();
+              }else{
+                c.fillStyle = that.textColor;
+              }
+              c.fillText(w, that.position.x + spacing, that.position.y);
+              spacing += c.measureText(w).width;
+            });
+          }else{
+  
+            if(that.currentCharacter === that.currentWordLength){
+              that.needUpdate = false;
+            }
+            this.holder.innerHTML = '';
+            word.forEach(function (w,index) {
+              var color = null
+              if(index > that.currentCharacter){
+                color = that.getRandomColor();
+              }else{
+                color = that.textColor;
+              }
+              that.holder.appendChild(that.generateSingleCharacter(color, w));
+            }); 
+          }
+          this.then = this.now - (this.delta % this.interval);
+        }
+    }
+  
+    this.restart = function () {
+      this.currentCharacter = 0;
+      this.needUpdate = true;
+    }
+  
+    function update(time) {
+      time++;
+      if(that.needUpdate){
+        that.updateCharacter(time);
+      }
+      requestAnimationFrame(update);
+    }
+  
+    this.writeWord(this.holder.innerHTML);
+  
+  
+    console.log(this.currentWord);
+    update(time);
+  }
+  
+  
+  
+  
+  var headline = document.getElementById('headline');
+  var text = document.getElementById('text');
+  var shuffler = document.getElementById('shuffler');
+  
+  var headText = new WordShuffler(headline,{
+    textColor : '#fff',
+    timeOffset : 18,
+    mixCapital : true,
+    mixSpecialCharacters : true
+  });
+  
+  var pText = new WordShuffler(text,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  
+  var buttonText = new WordShuffler(shuffler,{
+    textColor : 'tomato',
+    timeOffset : 10
+  });
+  
+  
+  
+    shuffler.addEventListener('click',function () {
+      headText.restart();
+      pText.restart();
+      buttonText.restart();
     });
